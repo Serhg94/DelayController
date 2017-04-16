300c1d781df46bdc9d081c389ff6c023cba0d6d4
Sergey Lobarev <Serhg94@mail.ru>
2017-04-16 14:00:17 +0300

    Улучшен вывод на дисплей, сделана переинициализация дисплея, если он не занят после отправки команды, поправки в алгоритме

--- PlasmaController/PlasmaController.c
+++ PlasmaController/PlasmaController.c
@@ -24,19 +24,29 @@ bool emerg_stop = 0;
 volatile bool torch_on = 0;
 volatile bool arc_on_out = 0;
 volatile bool rele_out = 0;
+volatile bool manual_mode = 0;
 
 volatile unsigned long delay_start_time;
 volatile short delay_start_ena = 0; //0 - выключена, 1- прогрев, 2 - прожиг
 
-char stringOne[16];
-char stringTwo[16];
+char stringOne[17];
+char stringTwo[17];
 short display_mutex;
 volatile short display_changed;
 
 //функция обработчик внешнего прерывания INT0- ARC_ON-вход
 ISR( INT0_vect )
 {
-
+	if (PIND & (1 << PIND2))
+	{
+		manual_mode = 1;
+		display_changed|=4;
+	}
+	else
+	{
+		manual_mode = 0;
+		display_changed|=4;
+	}
 }
 //функция обработчик внешнего прерывания INT1- torch_ON-вход
 ISR( INT1_vect )
@@ -68,6 +78,108 @@ ISR( INT1_vect )
 	//int An = ADCH; 
 	//return (An<<8) + An_pin;
 //}
+void displayRefrash()
+{
+	//если что то изменилось, либо надо показывать обратный отсчет
+	if ((display_changed)||(delay_start_ena>0))
+	if (!display_mutex)
+	{
+		//lcdClear();
+		if ((delay_start_ena==1)&&(manual_mode==0))
+		{
+			int d1=warm_delay-(float)millis()/1000;
+
+			//float f1 = warm_delay-millis()/1000;
+			//int d1 = f1;            // Get the integer part (678).
+			//float f2 = f1 - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
+			//int d2 = trunc(f2 * 10);   // Turn into integer (123).
+			sprintf(stringOne, " PO PEB:%3d %3d ", (int)warm_delay, d1);
+		}
+		else if (delay_start_ena==2)
+		{
+			//int d1=set_delay-(float)millis()/1000;
+
+			float f1 = set_delay-(float)millis()/1000;
+			int d1 = f1;            // Get the integer part (678).
+			float f2 = f1 - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
+			int d2 = trunc(f2 * 10);   // Turn into integer (123).
+			sprintf(stringOne, " PO PEB:%3d  %1d.%1d", (int)warm_delay, d1, d2);
+		}
+		else
+			sprintf(stringOne, " PO PEB:%3d     ", (int)warm_delay);
+		stringOne[0]=P_CHAR_CODE;
+		stringOne[3]=G_CHAR_CODE;
+		//lcdPuts(stringOne);
+		//lcdGotoXY(1,0);
+		int d1 = set_delay;            // Get the integer part (678).
+		float f2 = set_delay - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
+		int d2 = trunc(f2 * 10);   // Turn into integer (123).
+		sprintf(stringTwo, " PO   :%1d.%01d      ", d1, d2);
+		stringTwo[0]=P_CHAR_CODE;
+		stringTwo[3]=J_CHAR_CODE;
+		stringTwo[4]=I_CHAR_CODE;
+		stringTwo[5]=G_CHAR_CODE;
+		//lcdPuts(stringOne);
+		if (arc_on_out)
+		{
+			//lcdGotoXY(1,11);
+			stringTwo[15]=CHAR_THUNDER_CODE;
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		if (torch_on)
+		{
+			//lcdGotoXY(1,14);
+			stringTwo[13]=TORCH_CHAR_CODE;
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		if (delay_start_ena==1)
+		{
+			//lcdGotoXY(0,11);
+			stringTwo[11]=WARM_CHAR_CODE;
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		if (delay_start_ena==2)
+		{
+			//lcdGotoXY(0,11);
+			stringTwo[11]=BURN_CHAR_CODE;
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		if((emerg_stop)&&(delay_start_ena!=2))
+		{
+			//lcdGotoXY(0,14);
+			stringOne[13]='E';
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		if((manual_mode)&&(delay_start_ena!=2))
+		{
+			//lcdGotoXY(0,14);
+			stringOne[15]='P';
+			//stringOne[1]=0;
+			//lcdPuts(stringOne);
+		}
+		stringOne[16]=0;
+		stringTwo[16]=0;
+		if (!lcdGotoXY(0,0))
+		{
+			lcdInit();
+			display_changed = 1;
+		}
+		lcdPuts(stringOne);
+		if (!lcdGotoXY(1,0))
+		{
+			lcdInit();
+			display_changed = 1;
+		}
+		lcdPuts(stringTwo);
+		display_changed=0;
+	}
+}
+
 
 ISR (TIMER1_COMPA_vect)
 {
@@ -122,6 +234,16 @@ void init()
 	SetBit(DOWN_OUT_DDR, UP_OUT);
 	SetBit(DOWN_OUT_PORT, UP_OUT);
 	
+	//инициализация начальных значений
+	if (PIND & (1 << PIND3))
+		torch_on = 1;
+	else
+		torch_on = 0;
+	if (PIND & (1 << PIND2))
+		manual_mode = 1;
+	else
+		manual_mode = 0;
+	
 	//вход E_STOP
 	ClearBit(E_STOP_IN_DDR, E_STOP_IN);
 	SetBit(E_STOP_IN_PORT, E_STOP_IN);
@@ -142,70 +264,17 @@ void init()
 	ENC_InitEncoder();
 	
 	lcdLoadCharacterf(CHAR_THUNDER_CODE, char_thunder);
-	lcdLoadCharacterf(UP_ARROW_CODE, up_arrow);
-	lcdLoadCharacterf(DOWN_ARROW_CODE, down_arrow);
 	lcdLoadCharacterf(TORCH_CHAR_CODE, torch_char);
-	lcdLoadCharacterf(LOCK_CHAR_CODE, lock_char);
-	lcdLoadCharacterf(E_CHAR_CODE, e_char);
 	lcdLoadCharacterf(WARM_CHAR_CODE, warm_char);
 	lcdLoadCharacterf(BURN_CHAR_CODE, burn_char);
+	lcdLoadCharacterf(P_CHAR_CODE, p_char);
+	lcdLoadCharacterf(G_CHAR_CODE, g_char);
+	lcdLoadCharacterf(J_CHAR_CODE, j_char);
+	lcdLoadCharacterf(I_CHAR_CODE, i_char);
 	
 	display_mutex = 0;
 	display_changed = 3;
 	sei();
-}
-
-void displayRefrash()
-{
-	if (display_changed)
-	if (!display_mutex)
-	{
-		lcdClear();
-		sprintf(stringOne, "WARM: %d\0", warm_delay);
-		lcdPuts(stringOne);
-		lcdGotoXY(1,0);
-		int d1 = set_delay;            // Get the integer part (678).
-		float f2 = set_delay - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
-		int d2 = trunc(f2 * 10);   // Turn into integer (123).
-		sprintf(stringOne, "BURN: %d.%01d\0", d1, d2);
-		lcdPuts(stringOne);
-		if (arc_on_out)
-		{
-			lcdGotoXY(1,11);
-			stringOne[0]=CHAR_THUNDER_CODE;
-			stringOne[1]=0;
-			lcdPuts(stringOne);
-		}
-		if (torch_on)
-		{
-			lcdGotoXY(1,14);
-			stringOne[0]=TORCH_CHAR_CODE;
-			stringOne[1]=0;
-			lcdPuts(stringOne);
-		}
-		if (delay_start_ena==1)
-		{
-			lcdGotoXY(0,11);
-			stringOne[0]=WARM_CHAR_CODE;
-			stringOne[1]=0;
-			lcdPuts(stringOne);
-		}
-		if (delay_start_ena==2)
-		{
-			lcdGotoXY(0,11);
-			stringOne[0]=BURN_CHAR_CODE;
-			stringOne[1]=0;
-			lcdPuts(stringOne);
-		}
-		if(emerg_stop)
-		{
-			lcdGotoXY(0,14);
-			stringOne[0]=E_CHAR_CODE;
-			stringOne[1]=0;
-			lcdPuts(stringOne);
-		}
-		display_changed=0;
-	}
 }
 
 
@@ -264,8 +333,8 @@ void logicProcess()
 				display_changed|=4;
 			}
 		}
-		//после задержки на прогрев - включаем реле и задержку на прожиг
-		if ((delay_start_ena==1)&&(millis()>warm_delay*1000))
+		//после задержки на прогрев - включаем реле и задержку на прожиг только НЕ в ручном режиме
+		if ((delay_start_ena==1)&&(manual_mode==0)&&(millis()>warm_delay*1000))
 		{
 			delay_start_ena=2;
 			millis_reset();
